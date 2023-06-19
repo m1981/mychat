c52b3bd8a0da064fb7eed78d7f587bf729cfd2d1
Michal <m.nakiewicz@gmail.com>
2023-06-19 18:33:13 +0200

    models cleanup

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -19,29 +19,20 @@ export const modelOptions: ModelOptions[] = [
   'gpt-3.5-turbo',
   'gpt-4',
   'gpt-4-32k',
-  // 'gpt-3.5-turbo-0301',
-  // 'gpt-4-0314',
-  // 'gpt-4-32k-0314',
 ];
 
 export const defaultModel = 'gpt-4';
 
 export const modelMaxToken = {
   'gpt-3.5-turbo': 4096,
-  'gpt-3.5-turbo-0301': 4096,
   'gpt-4': 8192,
-  'gpt-4-0314': 8192,
   'gpt-4-32k': 32768,
-  'gpt-4-32k-0314': 32768,
 };
 
 export const modelCost = {
   'gpt-3.5-turbo': { price: 0.002, unit: 1000 },
-  'gpt-3.5-turbo-0301': { price: 0.002, unit: 1000 },
   'gpt-4': { price: 0.03, unit: 1000 },
-  'gpt-4-0314': { price: 0.03, unit: 1000 },
   'gpt-4-32k': { price: 0.06, unit: 1000 },
-  'gpt-4-32k-0314': { price: 0.06, unit: 1000 },
 };
 
 export const defaultUserMaxToken = 8192;
