f3d407faae7bf1fdf361aed49d30b8e79a8d2279
Michal <m.nakiewicz@gmail.com>
2023-06-22 11:12:48 +0200

    Update ChatFolder.tsx

--- src/components/Menu/ChatFolder.tsx
+++ src/components/Menu/ChatFolder.tsx
@@ -233,9 +233,6 @@ const ChatFolder = ({
           className='flex text-gray-300'
           onClick={(e) => e.stopPropagation()}
         >
-          <!-- <button className='p-1 hover:text-white' onClick={openDirectoryPicker}>
-            Open Directory
-          </button> -->
           {isDelete || isEdit ? (
             <>
               <button className='p-1 hover:text-white' onClick={handleTick}>
