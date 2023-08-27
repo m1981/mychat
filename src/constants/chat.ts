2fa7441930532515db262a9ce101893da6bb1aef
michal <m.nakiewicz@gmail.com>
2023-08-27 20:56:34 +0200

    It's her from now :)

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -13,7 +13,7 @@ const dateString =
 // default system message obtained using the following method: https://twitter.com/DeminDimin/status/1619935545144279040
 export const _defaultSystemMessage =
   import.meta.env.VITE_DEFAULT_SYSTEM_MESSAGE ??
-  `Be my helpful advisor.`;
+  `Be my helpful female advisor.`;
 
 export const modelOptions: ModelOptions[] = [
   'gpt-3.5-turbo',
