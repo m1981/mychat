3a5b6846b6c32418c4e10406096625b26bf9643a
Michal <m.nakiewicz@gmail.com>
2023-11-07 22:59:06 +0100

    Models options updated

--- src/types/chat.ts
+++ src/types/chat.ts
@@ -50,14 +50,10 @@ export interface Folder {
   color?: string;
 }
 
-export type ModelOptions = 'gpt-4' 
-  | 'gpt-4-1106-preview'
-  | 'gpt-4-32k' 
-  | 'gpt-3.5-turbo-16k'
-  | 'gpt-3.5-turbo';
-// | 'gpt-3.5-turbo-0301';
-// | 'gpt-4-0314'
-// | 'gpt-4-32k-0314'
+export type ModelOptions = 'gpt-3.5-turbo' 
+  | 'gpt-3.5-turbo-1106',
+  | 'gpt-4',
+  | 'gpt-4-1106-preview';
 
 export interface LocalStorageInterfaceV0ToV1 {
   chats: ChatInterface[];
