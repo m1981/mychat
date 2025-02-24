75b1634b9209207616b6b658ebb127d2942652cf
Michal <m.nakiewicz@gmail.com>
2025-02-24 21:08:25 +0000

    NewChat: Light theme fix

--- src/components/Menu/NewChat.tsx
+++ src/components/Menu/NewChat.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import useStore from '@store/store';
-
 import PlusIcon from '@icon/PlusIcon';
-
 import useAddChat from '@hooks/useAddChat';
 
 const NewChat = ({ folder }: { folder?: string }) => {
@@ -13,26 +11,29 @@ const NewChat = ({ folder }: { folder?: string }) => {
 
   return (
     <a
-      className={`flex flex-1 items-center rounded-md hover:bg-gray-500/10 transition-all duration-200 text-white text-sm flex-shrink-0 ${
-        generating
-          ? 'cursor-not-allowed opacity-40'
-          : 'cursor-pointer opacity-100'
-      } ${
-        folder ? 'justify-start' : 'py-2 px-2 gap-3 mb-2 border border-white/20'
-      }`}
+      className={`
+        flex flex-1 items-center rounded-md 
+        hover:bg-gray-500/10 transition-all duration-200 
+        text-gray-800 dark:text-gray-100 text-sm flex-shrink-0
+        ${generating ? 'cursor-not-allowed opacity-40' : 'cursor-pointer opacity-100'}
+        ${folder ? 'justify-start' : 'p-2 gap-3 mb-2 border border-gray-200 dark:border-gray-600'}
+      `}
       onClick={() => {
         if (!generating) addChat(folder);
       }}
       title={folder ? String(t('newChat')) : ''}
     >
       {folder ? (
-        <div className='max-h-0 parent-sibling-hover:max-h-10 hover:max-h-10 parent-sibling-hover:py-2 hover:py-2 px-2 overflow-hidden transition-all duration-200 delay-500 text-sm flex gap-3 items-center text-gray-100'>
+        <div className="max-h-0 parent-sibling-hover:max-h-10 hover:max-h-10
+                       parent-sibling-hover:py-2 hover:py-2 px-2
+                       overflow-hidden transition-all duration-200 delay-500
+                       flex gap-3 items-center">
           <PlusIcon /> {t('newChat')}
         </div>
       ) : (
         <>
           <PlusIcon />
-          <span className='inline-flex text-white text-sm'>{t('newChat')}</span>
+          <span className="inline-flex">{t('newChat')}</span>
         </>
       )}
     </a>
