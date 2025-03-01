25543a4bbe8b69dbf685f7bca4402d12d4db9ead
Michal <m.nakiewicz@gmail.com>
2025-03-01 20:27:23 +0000

    missing console added again

--- vite.config.ts
+++ vite.config.ts
@@ -61,8 +61,8 @@ export default defineConfig({
     minify: 'terser',
     terserOptions: {
       compress: {
-        drop_console: true,
-        drop_debugger: true
+        drop_console: false,
+        drop_debugger: false
       }
     },
 
