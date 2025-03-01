06b7afdb2e9a3ae22f440cc44416d0cae8cd81a0
Michal <m.nakiewicz@gmail.com>
2025-03-01 23:01:32 +0000

    tests added 3, 21 test but NO stream SSE test working  yet

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,8 +5,9 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    globals: true,
     environment: 'jsdom',
+    globals: true,
+
     setupFiles: ['./vitest.setup.ts'],
     coverage: {
       provider: 'v8',
