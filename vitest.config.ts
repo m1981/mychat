d265e0246e01b104ae9331e8f4c5a426c32d87ac
Michal <m.nakiewicz@gmail.com>
2025-05-05 14:51:46 +0200

    aliases added

--- vitest.config.ts
+++ vitest.config.ts
@@ -94,6 +94,8 @@ export default defineConfig({
       '@constants': path.resolve(__dirname, './src/constants'),
       '@utils': path.resolve(__dirname, './src/utils'),
       '@models': path.resolve(__dirname, './src/config/models'),
+      '@api': path.resolve(__dirname, './src/api'),
+      '@components': path.resolve(__dirname, './src/components'),
     },
   },
 });
