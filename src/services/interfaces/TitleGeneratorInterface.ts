a896dde9fe50d5ef8c2eed5224ecb072c9120c3b
Michal <m.nakiewicz@gmail.com>
2025-06-23 18:30:38 +0200

    unused file removed

--- src/services/interfaces/TitleGeneratorInterface.ts
+++
@@ -1,5 +0,0 @@
-   import { MessageInterface, ModelConfig } from '@type/chat';
-   
-   export interface TitleGeneratorInterface {
-     generateChatTitle: (messages: MessageInterface[], config: ModelConfig) => Promise<string>;
-   }
